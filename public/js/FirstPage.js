08b48f30859e273d67fe17d06fceced33a82b20f
RatchapongT <ratchapong.t@gmail.com>
2015-10-29 08:22:41 -0400

    Calendar fix

--- public/js/FirstPage.js
+++ public/js/FirstPage.js
@@ -1,72 +1,86 @@
 $(document).ready(function () {
 
 
-    var schoolCounter = 0;
-    var sleepCounter = 0;
-    var tiredCounter = 0;
-    var relieverCounter = 0;
-
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1;
-    var yyyy = today.getFullYear();
-
-    var today = $(".fc-today");
-    var schoolIcon = $("<div class='schoolIcon'></div>");
-    var sleepIcon = $("<div class='sleepIcon'></div>");
-    var tiredIcon = $("<div class='tiredIcon'></div>");
-    var relieverIcon = $("<div class='relieverIcon'></div>");
-
     $("#pen").click(function (e) {
-        today = $(".fc-today");
-        if (schoolCounter % 2 == 0) {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+
+        var today = $(".fc-today");
+        var schoolIcon = $("<div class='schoolIcon'></div>");
+        var sleepIcon = $("<div class='sleepIcon'></div>");
+        var tiredIcon = $("<div class='tiredIcon'></div>");
+        var relieverIcon = $("<div class='relieverIcon'></div>");
+
+        if (today.find(".schoolIcon").length == 0) {
             $("#pen").attr('src', 'img/schoolFill.png');
             today.append(schoolIcon);
-            schoolCounter++;
         } else {
             $("#pen").attr('src', 'img/schoolUnfill.png');
             today.find(".schoolIcon").remove();
-            schoolCounter++;
         }
     });
 
     $("#sleep").click(function (e) {
-        today = $(".fc-today");
-        if (sleepCounter % 2 == 0) {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+
+        var today = $(".fc-today");
+        var schoolIcon = $("<div class='schoolIcon'></div>");
+        var sleepIcon = $("<div class='sleepIcon'></div>");
+        var tiredIcon = $("<div class='tiredIcon'></div>");
+        var relieverIcon = $("<div class='relieverIcon'></div>");
+        if (today.find(".sleepIcon").length == 0) {
             $("#sleep").attr('src', 'img/sleepFill.png');
-            console.log(today);
             today.append(sleepIcon);
-            sleepCounter++;
+
         } else {
             $("#sleep").attr('src', 'img/sleepUnfill.png');
             today.find(".sleepIcon").remove();
-            sleepCounter++;
         }
     });
 
     $("#tired").click(function (e) {
-        today = $(".fc-today");
-        if (tiredCounter % 2 == 0) {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+
+        var today = $(".fc-today");
+        var schoolIcon = $("<div class='schoolIcon'></div>");
+        var sleepIcon = $("<div class='sleepIcon'></div>");
+        var tiredIcon = $("<div class='tiredIcon'></div>");
+        var relieverIcon = $("<div class='relieverIcon'></div>");
+        if (today.find(".tiredIcon").length == 0) {
             $("#tired").attr('src', 'img/limitFill.png');
             today.append(tiredIcon);
-            tiredCounter++;
+
         } else {
             $("#tired").attr('src', 'img/limitUnfill.png');
             today.find(".tiredIcon").remove();
-            tiredCounter++;
         }
     });
 
     $("#reliever").click(function (e) {
-        today = $(".fc-today");
-        if (relieverCounter % 2 == 0) {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+
+        var today = $(".fc-today");
+        var schoolIcon = $("<div class='schoolIcon'></div>");
+        var sleepIcon = $("<div class='sleepIcon'></div>");
+        var tiredIcon = $("<div class='tiredIcon'></div>");
+        var relieverIcon = $("<div class='relieverIcon'></div>");
+        if (today.find(".relieverIcon").length == 0) {
             $("#reliever").attr('src', 'img/releiveFill.png');
             today.append(relieverIcon);
-            relieverCounter++;
         } else {
             $("#reliever").attr('src', 'img/releiveUnfill.png');
             today.find(".relieverIcon").remove();
-            relieverCounter++;
         }
 
     });
